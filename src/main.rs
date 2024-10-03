231803d50deb0edae3f78cf0d718a102c39272ad
Bert de Bruijn <lpdebruijn@gmail.com>
2024-10-03 17:26:30 -0400

    Add experiments with variations of combine_intervals

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,46 @@
+use std::time::Instant;
 use intervalues;
 use rand::Rng;
 
 fn main() {
-    // nums = [Random().randint(0, 10) for _ in range(nr_intervals * 2)]
-    let mut input: Vec<[isize; 3]> = Vec::new();
     let mut rng = rand::thread_rng();
+
+
+    let mut input: Vec<[isize; 3]> = Vec::new();
     for _ in 0..1000000 {
         input.push([rng.gen_range(0..10), rng.gen_range(0..10), 1])
     }
+    let before = Instant::now();
+    let hi = intervalues::combine_intervals_values(input);
+    let after = Instant::now();
+    println!("{:?} in {:?}", hi, after - before);
+
+
+    let mut input: Vec<[isize; 2]> = Vec::new();
+    for _ in 0..1000000 {
+        input.push([rng.gen_range(0..10), rng.gen_range(0..10)])
+    }
+    let before = Instant::now();
     let hi = intervalues::combine_intervals(input);
-    println!("{:?}", hi);
+    let after = Instant::now();
+    println!("{:?} in {:?}", hi, after - before);
+
+    let mut input: Vec<[f64; 2]> = Vec::new();
+    for _ in 0..1000000 {
+        input.push([rng.gen_range(0..10) as f64, rng.gen_range(0..10) as f64])
+    }
+    let before = Instant::now();
+    let hi = intervalues::combine_intervals_flt(input);
+    let after = Instant::now();
+    println!("{:?} in {:?}", hi, after - before);
+
+    let mut input: Vec<[f64; 3]> = Vec::new();
+    for _ in 0..1000000 {
+        input.push([rng.gen_range(0..10) as f64, rng.gen_range(0..10) as f64, 1.0])
+    }
+    let before = Instant::now();
+    let hi = intervalues::combine_intervals_flt_values(input);
+    let after = Instant::now();
+    println!("{:?} in {:?}", hi, after - before);
+
 }
