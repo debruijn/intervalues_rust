b5b0a01a5bf7b20c372f8c7f130c5ab8cdbf2508
Bert de Bruijn <lpdebruijn@gmail.com>
2024-10-06 15:13:04 -0400

    Update base_interval with new features

--- src/base_interval.rs
+++ src/base_interval.rs
@@ -43,6 +43,14 @@ impl BaseInterval {
         self.val
     }
 
+    pub fn len(self) -> Number {
+        self.ub - self.lb
+    }
+
+    pub fn get_total_value(self) -> Number {
+        self.get_value() * self.len()
+    }
+
     pub fn contains(self, num: Number) -> bool {
         if (num >= self.lb) & (num <= self.ub) {
             true
@@ -67,9 +75,55 @@ impl BaseInterval {
         }
     }
 
-    // Next steps:
-    // - Test in Python
-    // - Add wrapper around Vec<BaseInterval> as result from combine
-    // - Return as Counter, Set, Meter
+    pub fn superset(self, other: BaseInterval) -> bool {
+        if (other.ub <= self.ub) && (other.lb >= self.lb) {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn subset(self, other: BaseInterval) -> bool {
+        other.superset(self)
+    }
+
+    pub fn left_overlaps(self, other: BaseInterval) -> bool {
+        if (self.lb <= other.lb) & (self.ub <= other.ub) {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn right_overlaps(self, other: BaseInterval) -> bool {
+        other.left_overlaps(self)
+    }
+
+    pub fn overlaps(self, other: BaseInterval) -> bool {
+        self.left_overlaps(other) || self.right_overlaps(other)
+    }
+
+    pub fn can_join_ign_value(self, other: &BaseInterval) -> bool {
+        if (self.ub == other.lb) || (other.ub == self.lb) {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn join_ign_value(self, other: &BaseInterval) -> BaseInterval {
+        let lb = if self.lb < other.lb {
+            self.lb
+        } else {
+            other.lb
+        };
+        let ub = if self.ub > other.ub {
+            self.ub
+        } else {
+            other.ub
+        };
+        BaseInterval::new(lb, ub, Number::from(1))
+    }
+
 
 }
