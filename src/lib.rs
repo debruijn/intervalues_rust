50034b61fb7c0d9b514530ea58d6f7ec50eccba1
Bert de Bruijn <lpdebruijn@gmail.com>
2024-10-08 21:25:20 -0400

    Follow cargo fmt: change use order in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,6 @@ mod base_interval;
 mod combine_intervals;
 mod interval_collection;
 
-pub use crate::combine_intervals::combine_intervals;
 pub use crate::base_interval::BaseInterval;
+pub use crate::combine_intervals::combine_intervals;
 pub use crate::interval_collection::IntervalCollection;
